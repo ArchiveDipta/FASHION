c1a9bfd4d85c246488f7550eff526f0f24e67912
ArchiveDipta <archivepradipta@gmail.com>
2026-06-01 22:57:53 +0700

    fix: register MulterModule with dest uploads to prevent undefined filenames

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
 
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      dest: './uploads',
+    }),
+  ],
   controllers: [
     ProductsController,
   ],
-
   providers: [
     ProductsService,
   ],
