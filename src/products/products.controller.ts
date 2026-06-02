29ffc9260aee44cd093fc99308a2e33b57a8e9eb
ArchiveDipta <archivepradipta@gmail.com>
2026-06-02 07:13:15 +0700

    fix: explicitly define diskStorage in products controller so image filenames are generated properly

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -12,10 +12,20 @@ import {
   ParseIntPipe,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 import { ApiBearerAuth, ApiTags, ApiConsumes, ApiQuery } from '@nestjs/swagger';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+
+const storage = diskStorage({
+  destination: './uploads',
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, `${uniqueSuffix}${extname(file.originalname)}`);
+  },
+});
 
 @ApiTags('Products')
 @ApiBearerAuth()
@@ -24,7 +34,7 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  @UseInterceptors(FilesInterceptor('image'))
+  @UseInterceptors(FilesInterceptor('image', 10, { storage }))
   @ApiConsumes('multipart/form-data')
   create(
     @Body() createProductDto: CreateProductDto,
@@ -55,7 +65,7 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  @UseInterceptors(FilesInterceptor('image'))
+  @UseInterceptors(FilesInterceptor('image', 10, { storage }))
   @ApiConsumes('multipart/form-data')
   update(
     @Param('id', ParseIntPipe) id: number,
