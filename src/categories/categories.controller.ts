6bc415a00aa1e88a598a3369376dbccf96cfe770
ArchiveDipta <archivepradipta@gmail.com>
2026-06-03 07:18:54 +0700

    Menambahkan role ADMIN ke CRUD Categories

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -11,21 +11,17 @@ import {
 } from '@nestjs/common';
 
 import { CategoriesService } from './categories.service';
-
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
 import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
 import { RolesGuard } from '../common/guards/role.guard';
-
 import { Roles } from '../common/decorators/role.decorator';
-
 import { Role } from '../common/enums/role.enum';
 
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Categories')
-@ApiBearerAuth('bearer')
 @Controller('categories')
 export class CategoriesController {
   constructor(
@@ -33,18 +29,11 @@ export class CategoriesController {
   ) {}
 
   @Post()
-  @UseGuards(
-    JwtAuthGuard,
-    RolesGuard,
-  )
+  @ApiBearerAuth('bearer')
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
-  create(
-    @Body()
-    dto: CreateCategoryDto,
-  ) {
-    return this.categoriesService.create(
-      dto,
-    );
+  create(@Body() dto: CreateCategoryDto) {
+    return this.categoriesService.create(dto);
   }
 
   @Get()
@@ -53,46 +42,26 @@ export class CategoriesController {
   }
 
   @Get(':id')
-  findOne(
-    @Param('id', ParseIntPipe)
-    id: number,
-  ) {
-    return this.categoriesService.findOne(
-      id,
-    );
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriesService.findOne(id);
   }
 
   @Put(':id')
-  @UseGuards(
-    JwtAuthGuard,
-    RolesGuard,
-  )
+  @ApiBearerAuth('bearer')
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
   update(
-    @Param('id', ParseIntPipe)
-    id: number,
-
-    @Body()
-    dto: UpdateCategoryDto,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateCategoryDto,
   ) {
-    return this.categoriesService.update(
-      id,
-      dto,
-    );
+    return this.categoriesService.update(id, dto);
   }
 
   @Delete(':id')
-  @UseGuards(
-    JwtAuthGuard,
-    RolesGuard,
-  )
+  @ApiBearerAuth('bearer')
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
-  remove(
-    @Param('id', ParseIntPipe)
-    id: number,
-  ) {
-    return this.categoriesService.remove(
-      id,
-    );
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriesService.remove(id);
   }
 }
